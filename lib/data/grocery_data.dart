ce6a9c902a8a24dc8cdd42d3b6ee0e6b569bd5cf
Shiffa Ur Rehman <22100167@lums.edu.pk>
2023-09-24 17:14:42 +0500

    home screen initalize and load with bloc

---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,53 @@
+class GroceryData {
+  static List<Map<String, dynamic>> groceryProducts = [
+    {
+      'id': 1,
+      'name': 'Fresh Apples',
+      'description': 'Crisp and juicy red apples',
+      'price': 2.99,
+      'imageUrl':
+          'https://cdn.britannica.com/22/187222-050-07B17FB6/apples-on-a-tree-branch.jpg',
+    },
+    {
+      'id': 2,
+      'name': 'Bananas',
+      'description': 'Ripe and yellow bananas',
+      'price': 1.99,
+      'imageUrl':
+          'https://th-thumbnailer.cdn-si-edu.com/4Nq8HbTKgX6djk07DqHqRsRuFq0=/1000x750/filters:no_upscale()/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/d5/24/d5243019-e0fc-4b3c-8cdb-48e22f38bff2/istock-183380744.jpg',
+    },
+    {
+      'id': 3,
+      'name': 'Whole Wheat Bread',
+      'description': 'Healthy whole wheat bread loaf',
+      'price': 3.49,
+      'imageUrl':
+          'https://www.31daily.com/wp-content/uploads/2021/04/md-100-Whole-Wheat-Bread-11-1-of-1-scaled.jpg',
+    },
+    {
+      'id': 4,
+      'name': 'Milk (1 Gallon)',
+      'description': 'Fresh and creamy whole milk',
+      'price': 2.79,
+      'imageUrl':
+          'https://img.freepik.com/free-vector/isolated-milk-box-cartoon-style_1308-117384.jpg?w=360',
+    },
+    {
+      'id': 5,
+      'name': 'Eggs (Dozen)',
+      'description': 'Farm-fresh eggs',
+      'price': 1.99,
+      'imageUrl':
+          'https://cdn.britannica.com/94/151894-050-F72A5317/Brown-eggs.jpg',
+    },
+    {
+      'id': 6,
+      'name': 'Spinach',
+      'description': 'Organic spinach leaves',
+      'price': 2.49,
+      'imageUrl':
+          'https://cdn.britannica.com/30/82530-050-79911DD4/Spinach-leaves-vitamins-source-person.jpg',
+    },
+    // Add more products as needed
+  ];
+}
